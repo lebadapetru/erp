8b39f0d3e03ab7f601723372259e593782ba0477
Lebada Petru Decebal <lebadap@gmail.com>
2021-02-14 16:21:03 +0200

    WIP add product form

---
+++ resources/components/forms/inputs/VSelect.vue
@@ -0,0 +1,29 @@
+<template>
+  <select class="custom-select form-control">
+    <option selected="selected">Open this select menu</option>
+    <option value="1">One</option>
+    <option value="2">Two</option>
+    <option value="3">Three</option>
+  </select>
+</template>
+
+<script>
+
+export default {
+  name: "VSelect",
+  components: {
+  },
+  setup() {
+    return {
+      options: [
+          'option1',
+          'option2'
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/products/AddProduct.vue
+++ resources/views/products/AddProduct.vue
@@ -1,126 +1,172 @@
 <template>
-  <!--begin::Card-->
-  <div class="card card-custom" id="kt_page_sticky_card">
-    <div class="card-header">
-      <div class="card-title">
-        <h3 class="card-label">Add product</h3>
-      </div>
-      <div class="card-toolbar">
-      </div>
-    </div>
-    <div class="card-body">
-      <!--begin::Form-->
-      <form class="form" id="kt_form">
-        <div class="row">
-          <div class="col-xl-8">
-            <div class="my-5">
-              <div class="form-group row">
-                <div class="col-12">
-                  <label>Title</label>
-                  <input class="form-control" type="text" value="Shoes" />
-                </div>
-              </div>
-              <div class="form-group row">
-                <div class="col-12">
-                  <label>Description</label>
-                  <VCKEditor />
-                </div>
-              </div>
-            </div>
-            <div class="separator separator-dashed my-10"></div>
-            <div class="my-5">
-              <h3 class="text-dark font-weight-bold mb-10">Media</h3>
-              <div class="form-group row">
-                <div class="col-12">
-                  <VDropZone />
-                  <span class="form-text text-muted">If you want your invoices addressed to a company. Leave blank to use your full name.</span>
-                </div>
-              </div>
-            </div>
-            <div class="separator separator-dashed my-10"></div>
-            <div class="my-5">
-              <h3 class="text-dark font-weight-bold mb-10">Pricing</h3>
-              <div class="form-group row">
-                <div class="col-6">
-                  <label>Price</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-										  <span class="input-group-text">
-											  RON
-                      </span>
-                    </div>
-                    <input type="text" class="form-control" value="15" placeholder="0.00" />
-                  </div>
-                </div>
 
-                <div class="col-6">
-                  <label>Compare at price</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">
-											  RON
-                      </span>
-                    </div>
-                    <input type="text" class="form-control" value="18" placeholder="0.00" />
-                  </div>
-                </div>
+  <form class="form">
+    <div style="display: flex; flex-flow: wrap row; justify-content: center;">
+      <div class="mt-4 ml-4" style="flex-flow: nowrap column;  flex: 2 2 auto;">
+
+        <div class="card card-custom mt-4">
+          <div class="card-body">
+            <div class="form-group row">
+              <div class="col-12">
+                <label>Title</label>
+                <input class="form-control" type="text" value="Shoes" />
               </div>
             </div>
-            <div class="separator separator-dashed my-10"></div>
-            <div class="my-5">
-              <h3 class="text-dark font-weight-bold mb-10">Inventory</h3>
-              <div class="form-group row">
-                <div class="col-6">
-                  <label>SKU (Stock Keeping Unit)</label>
-                  <input type="text" class="form-control" value="524" />
-                </div>
-
-                <div class="col-6">
-                  <label>Barcode (ISBN, UPC, GTIN, etc.)</label>
-                  <input type="text" class="form-control" value="1234567" />
-                </div>
-              </div>
-              <div class="form-group row">
-                <div class="col-12">
-                  <label>Quantity</label>
-                  <input type="number" class="form-control" placeholder="0" value="10" />
-                </div>
+            <div class="form-group row">
+              <div class="col-12">
+                <label>Description</label>
+                <VCKEditor />
               </div>
             </div>
-            <div class="separator separator-dashed my-10"></div>
-            <div class="my-5">
-              <h3 class="text-dark font-weight-bold mb-10">Variants</h3>
-              <div class="form-group row">
-                <div class="col-12">
-                  <div class="checkbox-inline">
-                    <label class="checkbox">
-                      <input type="checkbox" />
-                      <span></span>This product has multiple options, like different sizes or colors</label>
-                  </div>
-                </div>
-              </div>
-              <div class="form-group row">
-                <div class="col-6">
-                  <label>Option 1</label>
-                  <input type="text" class="form-control" placeholder="Size" value="" />
-                </div>
-
-                <div class="col-6">
-                  <label>Value</label>
-                  <input type="text" class="form-control" placeholder="Separate options with a comma" value="" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-4">
-
           </div>
         </div>
-      </form>
-      <!--end::Form-->
+
+        <div class="card card-custom mt-4">
+          <div class="card-header">
+            <div class="card-title">
+              <h3 class="card-label">Media</h3>
+            </div>
+            <div class="card-toolbar">
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="form-group row">
+              <div class="col-12">
+                <VDropZone />
+                <span class="form-text text-muted">If you want your invoices addressed to a company. Leave blank to use your full name.</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card card-custom mt-4">
+          <div class="card-header">
+            <div class="card-title">
+              <h3 class="card-label">Pricing</h3>
+            </div>
+            <div class="card-toolbar">
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="form-group row">
+              <div class="col-6">
+                <label>Price</label>
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                        <span class="input-group-text">
+                          RON
+                        </span>
+                  </div>
+                  <input type="text" class="form-control" value="15" placeholder="0.00" />
+                </div>
+              </div>
+
+              <div class="col-6">
+                <label>Compare at price</label>
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                        <span class="input-group-text">
+                          RON
+                        </span>
+                  </div>
+                  <input type="text" class="form-control" value="18" placeholder="0.00" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card card-custom mt-4">
+          <div class="card-header">
+            <div class="card-title">
+              <h3 class="card-label">Inventory</h3>
+            </div>
+            <div class="card-toolbar">
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="form-group row">
+              <div class="col-6">
+                <label>SKU (Stock Keeping Unit)</label>
+                <input type="text" class="form-control" value="524" />
+              </div>
+
+              <div class="col-6">
+                <label>Barcode (ISBN, UPC, GTIN, etc.)</label>
+                <input type="text" class="form-control" value="1234567" />
+              </div>
+            </div>
+            <div class="form-group row">
+              <div class="col-12">
+                <label>Quantity</label>
+                <input type="number" class="form-control" placeholder="0" value="10" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card card-custom mt-4">
+          <div class="card-header">
+            <div class="card-title">
+              <h3 class="card-label">Variants</h3>
+            </div>
+            <div class="card-toolbar">
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="form-group row">
+              <div class="col-12">
+                <div class="checkbox-inline">
+                  <label class="checkbox">
+                    <input type="checkbox" />
+                    <span></span>This product has multiple options, like different sizes or colors</label>
+                </div>
+              </div>
+            </div>
+            <div class="form-group row">
+              <div class="col-6">
+                <label>Option 1</label>
+                <input type="text" class="form-control" placeholder="Size" value="" />
+              </div>
+
+              <div class="col-6">
+                <label>Value</label>
+                <input type="text" class="form-control" placeholder="Separate options with a comma" value="" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="mt-4 ml-4" style="flex-flow: nowrap column; flex: 1 1 auto;">
+
+        <div class="card card-custom mt-4">
+          <div class="card-header">
+            <div class="card-title">
+              <h3 class="card-label">Product status</h3>
+            </div>
+            <div class="card-toolbar">
+                  <span class="switch switch-outline switch-icon switch-success">
+                      <label>
+                        <input type="checkbox" checked="checked" name="select" />
+                        <span></span>
+                      </label>
+                    </span>
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="form-group row">
+              <div class="col-12">
+                <VSelect />
+                <span class="form-text text-muted">If you want your invoices addressed to a company. Leave blank to use your full name.</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
-  </div>
-  <!--end::Card-->
+  </form>
 
   <teleport to="#toolbar">
     <!--begin::Button-->
@@ -190,12 +236,14 @@
 import { ref } from 'vue'
 import VCKEditor from "resources/components/forms/inputs/VCKEditor"
 import VDropZone from "resources/components/forms/inputs/VDropZone";
+import VSelect from "resources/components/forms/inputs/VSelect";
 
 export default {
   name: "AddProduct",
   components: {
     VCKEditor,
     VDropZone,
+    VSelect,
   },
   setup() {
     let isSubmitDropdownVisible = ref(false)
