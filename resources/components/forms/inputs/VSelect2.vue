f7af73052f551a226e60964ad526ccbcd0311b85
Lebada Petru Decebal <lebadap@gmail.com>
2021-02-17 18:27:17 +0200

    add sagalbot vue select plugin

---
+++ resources/components/forms/inputs/VSelect2.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-select :options="options" label="country" />
+</template>
+
+<script>
+import { onMounted } from 'vue'
+import vSelect from '../../../../node_modules/vue-select/src/index.js';
+
+export default {
+  name: "VSelect2",
+  components: {
+    vSelect
+  },
+  setup() {
+    onMounted(() => {
+      console.log(vSelect)
+    })
+    return {
+      options: [
+        'option1',
+        'option2'
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/products/AddProduct.vue
+++ resources/views/products/AddProduct.vue
@@ -1,9 +1,9 @@
 <template>
-
+  <!--begin::Add ProductForm-->
   <form class="form">
-    <div style="display: flex; flex-flow: wrap row; justify-content: center;">
-      <div class="mt-4 ml-4" style="flex-flow: nowrap column;  flex: 2 2 auto;">
-
+    <div class="row">
+      <div class="col-xl-8">
+        <!--begin::Title&Description-->
         <div class="card card-custom mt-4">
           <div class="card-body">
             <div class="form-group row">
@@ -20,13 +20,22 @@
             </div>
           </div>
         </div>
-
+        <!--end::Title&Description-->
+        <!--begin::Media-->
         <div class="card card-custom mt-4">
           <div class="card-header">
             <div class="card-title">
               <h3 class="card-label">Media</h3>
             </div>
             <div class="card-toolbar">
+              <a href="#">
+                Add media from url
+
+                <span>
+                  <i class="ki ki-bold-triangle-bottom icon-sm"></i>
+                </span>
+
+              </a>
             </div>
           </div>
           <div class="card-body">
@@ -38,7 +47,8 @@
             </div>
           </div>
         </div>
-
+        <!--end::Media-->
+        <!--begin::Pricing-->
         <div class="card card-custom mt-4">
           <div class="card-header">
             <div class="card-title">
@@ -75,7 +85,8 @@
             </div>
           </div>
         </div>
-
+        <!--end::Pricing-->
+        <!--begin::Inventory-->
         <div class="card card-custom mt-4">
           <div class="card-header">
             <div class="card-title">
@@ -96,15 +107,82 @@
                 <input type="text" class="form-control" value="1234567" />
               </div>
             </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="checkbox-inline">
+                  <label class="checkbox">
+                    <input type="checkbox" />
+                    <span></span>Track quantity
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="checkbox-inline">
+                  <label class="checkbox">
+                    <input type="checkbox" />
+                    <span></span>Continue selling when out of stock
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="card-footer">
+            <div class="card-title">
+              <h6 class="card-label">Quantity</h6>
+            </div>
+
             <div class="form-group row">
               <div class="col-12">
-                <label>Quantity</label>
-                <input type="number" class="form-control" placeholder="0" value="10" />
+                <label>Available</label>
+                  <input type="number" class="form-control" placeholder="0" value="0" />
               </div>
             </div>
           </div>
         </div>
+        <!--end::Inventory-->
+        <!--begin::Shipping-->
+        <div class="card card-custom mt-4">
+          <div class="card-header">
+            <div class="card-title">
+              <h3 class="card-label">Shipping</h3>
+            </div>
+            <div class="card-toolbar">
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="checkbox-inline">
+              <label class="checkbox">
+                <input type="checkbox" />
+                <span></span>This is a physical product
+              </label>
+            </div>
+          </div>
+          <div class="card-footer">
+            <div class="form-group row">
+              <div class="col-7">
+                <div class="row">
+                  <div class="col-9">
+                    <label>Weight</label>
+                    <input type="text" class="form-control" placeholder="0.0" value="0.0" />
+                  </div>
+                  <div class="col-3">
+                    <label>Measure unit</label>
+                    <VSelect />
+                  </div>
+                </div>
 
+              </div>
+            </div>
+          </div>
+          <div class="card-footer">
+            <span class="form-text text-muted">Customers won’t enter their shipping address or choose a shipping method when buying this product.</span>
+          </div>
+        </div>
+        <!--end::Shipping-->
+        <!--begin::Variants-->
         <div class="card card-custom mt-4">
           <div class="card-header">
             <div class="card-title">
@@ -119,7 +197,8 @@
                 <div class="checkbox-inline">
                   <label class="checkbox">
                     <input type="checkbox" />
-                    <span></span>This product has multiple options, like different sizes or colors</label>
+                    <span></span>This product has multiple options, like different sizes or colors
+                  </label>
                 </div>
               </div>
             </div>
@@ -136,14 +215,14 @@
             </div>
           </div>
         </div>
-
+        <!--end::Variants-->
       </div>
-      <div class="mt-4 ml-4" style="flex-flow: nowrap column; flex: 1 1 auto;">
-
+      <div class="col-xl-4">
+        <!--begin::Product status & visibility-->
         <div class="card card-custom mt-4">
           <div class="card-header">
             <div class="card-title">
-              <h3 class="card-label">Product status</h3>
+              <h3 class="card-label">Product status & visibility</h3>
             </div>
             <div class="card-toolbar">
                   <span class="switch switch-outline switch-icon switch-success">
@@ -162,11 +241,63 @@
               </div>
             </div>
           </div>
-        </div>
+          <div class="card-footer">
+            <div class="card-title">
+              <h6 class="card-label">Sales channels and apps</h6>
+            </div>
 
+            <div>
+              <div class="checkbox-inline mb-2">
+                <label class="checkbox">
+                  <input type="checkbox" checked />
+                  <span></span>Online store
+                </label>
+              </div>
+
+              <a href="#">Schedule availability</a>
+            </div>
+          </div>
+        </div>
+        <!--end::Product status & visibility-->
+        <!--begin::Organization-->
+        <div class="card card-custom mt-4">
+          <div class="card-header">
+            <div class="card-title">
+              <h3 class="card-label">Organization</h3>
+            </div>
+            <div class="card-toolbar">
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="form-group row">
+              <div class="col-12">
+                <label>Vendor</label>
+                <VSelect />
+              </div>
+            </div>
+          </div>
+          <div class="card-footer">
+            <div class="card-title">
+              <h6 class="card-label">Categories</h6>
+            </div>
+
+            <VSelect2 />
+            <span class="form-text text-muted">Add this product to a category so it’s easy to find in your store.</span>
+          </div>
+
+          <div class="card-footer">
+            <div class="card-title">
+              <h6 class="card-label">Tags</h6>
+            </div>
+
+            <VSelect />
+          </div>
+        </div>
+        <!--end::Organization-->
       </div>
     </div>
   </form>
+  <!--end::AddProductForm-->
 
   <teleport to="#toolbar">
     <!--begin::Button-->
@@ -237,6 +368,7 @@ import { ref } from 'vue'
 import VCKEditor from "resources/components/forms/inputs/VCKEditor"
 import VDropZone from "resources/components/forms/inputs/VDropZone";
 import VSelect from "resources/components/forms/inputs/VSelect";
+import VSelect2 from "resources/components/forms/inputs/VSelect2";
 
 export default {
   name: "AddProduct",
@@ -244,6 +376,7 @@ export default {
     VCKEditor,
     VDropZone,
     VSelect,
+    VSelect2,
   },
   setup() {
     let isSubmitDropdownVisible = ref(false)
@@ -268,7 +401,7 @@ export default {
 <style scoped>
 .dropdown-menu-right {
   position: absolute;
-  transform: translate3d(-72px, 38px, 0px);
+  transform: translate3d(-30px, 40px, 0px);
   top: 0;
   left: 0;
   will-change: transform;
